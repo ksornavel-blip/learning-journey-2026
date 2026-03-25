0871542d4be91fc00d22527f121ccb26a544bfcb
ksornavel-blip <ksornavel@gmail.com>
2026-03-25 15:45:19 +0530

    Delete PatientAPI directory

--- PatientAPI/Models/Patient.cs
+++
@@ -1,9 +0,0 @@
-namespace PatientAPI.Models;
-
-public record Patient
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int Age { get; set; }
-    public string Condition { get; set; } = string.Empty;
-}
